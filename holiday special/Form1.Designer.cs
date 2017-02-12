5107ece4de06d6365574875b03a6da6cade639b6
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 13:10:52 -0500

    Add project files.

---
+++ holiday special/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace holiday_special
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnHonolulu = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.Color.MistyRose;
+            this.lblOutput.Location = new System.Drawing.Point(12, 20);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(247, 94);
+            this.lblOutput.TabIndex = 0;
+            this.lblOutput.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Disney";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnHonolulu
+            // 
+            this.btnHonolulu.Location = new System.Drawing.Point(159, 144);
+            this.btnHonolulu.Name = "btnHonolulu";
+            this.btnHonolulu.Size = new System.Drawing.Size(100, 25);
+            this.btnHonolulu.TabIndex = 2;
+            this.btnHonolulu.Text = "Honolulu";
+            this.btnHonolulu.UseVisualStyleBackColor = true;
+            this.btnHonolulu.Click += new System.EventHandler(this.btnHonolulu_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(159, 204);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 25);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&XIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(15, 204);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 25);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clea&r";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnHonolulu);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnHonolulu;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ holiday special/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace holiday_special
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const string Disney = "Disney: 3 Days 2 Nights for $899";
+            lblOutput.Text = Disney;
+
+        }
+
+        private void btnHonolulu_Click(object sender, EventArgs e)
+        {
+            const string Honolulu = "Honolulu: One Week for $1,199";
+            lblOutput.Text = Honolulu;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
